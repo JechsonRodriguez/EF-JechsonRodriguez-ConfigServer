cb851db1d87a6aea539d54f13d4c47eac701e083
unknown <a18101601@idat.edu.pe>
2022-08-19 00:13:02 -0500

    configserver

---
+++ src/test/java/com/idat/EFLuisFernandezConfigServer/EfLuisFernandezConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EFJechsonRodriguezConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfJechsonRodriguezConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
